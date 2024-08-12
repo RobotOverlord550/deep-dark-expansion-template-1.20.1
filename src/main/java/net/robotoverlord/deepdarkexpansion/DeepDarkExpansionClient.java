d821a6de2470bd5e64b0e3c90eef00c0451fda5c
RobotOverlord550 <erniewhitmarsh3@gmail.com>
2024-08-12 10:53:31 -0400

    stugg

---
+++ src/main/java/net/robotoverlord/deepdarkexpansion/DeepDarkExpansionClient.java
@@ -0,0 +1,10 @@
+package net.robotoverlord.deepdarkexpansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeepDarkExpansionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
